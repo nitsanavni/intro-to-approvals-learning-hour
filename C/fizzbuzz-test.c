aae75d598735f24c46804f0cbabc51b7fd70706e
Nitsan Avni <nitsanav@gmail.com>
2023-01-13 07:31:57 +0000

    rotation in the code

--- C/fizzbuzz-test.c
+++ C/fizzbuzz-test.c
@@ -1,3 +1,15 @@
 #include <stdio.h>
 
 int main() { printf("hello world!"); }
+
+// --> typing --> talking --> next -->
+// Nitsan
+// Dietmar
+// Stephanie
+// Konrad
+// Jonas
+// Emily
+// Christian
+// Yunfeng
+// Pascal
+// Tom
